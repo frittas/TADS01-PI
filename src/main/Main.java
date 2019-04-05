9316529d93e1600e33a9161298e7995983658ad7
Rafael Freitas <raugf@br.ibm.com>
2019-04-05 20:34:31 -0300

    project first commit

---
+++ src/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+public class Main {
+
+    public static void main(String[] args) {
+        initGame();
+    }
+
+
+    public static void initGame(){
+    }
+
+    public static void initVariables(){
+    }
+}
